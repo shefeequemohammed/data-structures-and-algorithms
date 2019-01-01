ecdb21553e731a7403a63e1f55a7cc8721d9cc52
Shefeeque Mohammed <34381191+shefeequemohammed@users.noreply.github.com>
2019-01-01 22:46:46 +0400

    Create LinkedList.java
    Singly Linked list

---
+++ Java/LinkedList.java
@@ -0,0 +1,58 @@
+public class LinkedList{
+    private static Node head;
+    private static int numOfNodes = 0;
+    
+    public LinkedList(String data){
+        head = new Node(data);
+    }
+    
+    public static void main(String args[]){
+        
+        LinkedList l1 = new LinkedList("Node1");
+        l1.addNodeToHead("Node2");
+        l1.addNodeToHead("Node3");
+        l1.printList();
+        
+    }
+    
+    public void addNodeToHead(String data){
+        Node temp = head;
+        head = new Node(data);
+        head.setNext(temp);
+        numOfNodes+=1;
+    }
+    
+    public static void printList()
+	{
+		Node temp = head;
+		while(temp != null)
+		{
+			System.out.println(temp.getData());
+			temp = temp.getNext();
+		}
+	}
+}
+
+
+class Node{
+    private String data;
+    private Node next;
+    
+    public Node(String data){
+        this.data = data;
+    }
+    
+    public void setData(String data){
+        this.data = data;
+    }
+    
+    public void setNext(Node next){
+        this.next = next;
+    }
+    public String getData(){
+        return this.data;
+    }
+     public Node getNext(){
+        return this.next;
+    }
+}
